f8e60774ee72b991e1041ccb388ee440176e7955
dmoralgo <dmoralgo@EMEAL.NTTDATA.COM>
2024-06-19 12:23:27 -0600

    Creacion del controlador de Course

---
+++ microservice-course/src/main/java/com/microservice/controller/CourseController.java
@@ -0,0 +1,34 @@
+package com.microservice.controller;
+
+import com.microservice.entities.Course;
+import com.microservice.service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/courses")
+public class CourseController {
+
+    @Autowired
+    private CourseService courseService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void save(@RequestBody Course course){
+        courseService.save(course);
+    }
+
+    @GetMapping("/{id}")
+    public Course findById(@PathVariable Long id){
+        return courseService.findById(id);
+    }
+
+    @GetMapping
+    public List<Course> findAll(){
+        return courseService.findAll();
+    }
+
+}
