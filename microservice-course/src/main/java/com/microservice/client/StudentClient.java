dfb470fdbd3d9421b5e3b2a201405a8588f786b3
dmoralgo <dmoralgo@EMEAL.NTTDATA.COM>
2024-06-19 17:50:17 -0600

    se relizo la configuracion del ConfigServe donde se implementaron las configuraciones de todos nuestros micro servicios y se modificaron los que estaban en cada micro servio

--- microservice-course/src/main/java/com/microservice/client/StudentClient.java
+++ microservice-course/src/main/java/com/microservice/client/StudentClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "msvc-student", url = "http://localhost:8090/api/students")
+@FeignClient(name = "msvc-student", url = "http://localhost:8080/api/students")
 public interface StudentClient {
 
     @GetMapping("/course/{courseId}")
