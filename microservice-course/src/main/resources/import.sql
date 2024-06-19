ae01b6bbc07c65856c701ea9dd35753fce53aebe
dmoralgo <dmoralgo@EMEAL.NTTDATA.COM>
2024-06-19 16:39:40 -0600

    creacion de los archivos de importacion para las pruebas

---
+++ microservice-course/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO courses (name, teacher) VALUES ('Java', 'Profesor A');
+INSERT INTO courses (name, teacher) VALUES ('Spring', 'Profesor B');
+INSERT INTO courses (name, teacher) VALUES ('Microservices', 'Profesor C');
+INSERT INTO courses (name, teacher) VALUES ('Docker', 'Profesor D');
+INSERT INTO courses (name, teacher) VALUES ('Kubernetes', 'Profesor E');
---
+++ microservice-student/src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO students (name,last_name, email, course_id) VALUES ('John', 'Doe', 'jon@gmail.com', 1);
+INSERT INTO students (name,last_name, email, course_id) VALUES ('Jane', 'Doe', 'Jane@emeal.com', 2);
+INSERT INTO students (name,last_name, email, course_id) VALUES ('Doe', 'Doe', 'Doe@gmai.com', 1);
+INSERT INTO students (name,last_name, email, course_id) VALUES ('Alice', 'Doe', 'Alice@emeal.com', 2);
+INSERT INTO students (name,last_name, email, course_id) VALUES ('Bob', 'Doe', 'Bob@gmail.com', 3);
+
+
